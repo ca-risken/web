4beeb1c30c0cdb0ca48231a05a68ebcf93de1525
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-10-20 16:21:00 +0900

    no project dashboard

--- src/view/Dashboard.vue
+++ src/view/Dashboard.vue
@@ -197,16 +197,16 @@ export default {
         notification: [],   // 5. Setting Alert Notification
       },
       status: {
-        alert: '0',
-        finding: '0',
+        alert: '-',
+        finding: '-',
         compSettingRate: '-',
-        imcompSetting: '0',
+        imcompSetting: '-',
         riskFactor: [],
         risk: {
-          icon: '',
-          color: '',
-          description: '',
-          detail: '',
+          icon: 'mdi-map-marker-question-outline',
+          color: 'grey',
+          description: 'There is nothing to display.',
+          detail: 'Please reload after selecting the project.',
         }
       },
 
@@ -245,6 +245,9 @@ export default {
     }
   },
   async mounted() {
+    if (!this.$store.state.project.project_id) {
+      return false
+    }
     this.nowUnix = Math.floor(new Date() / 1000)
     this.oneMonthAgoUnix = Math.floor(new Date().setMonth(new Date().getMonth() - 1) / 1000)
 
