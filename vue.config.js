7cd167f2e0c94b9a332b1393be7b123de9ac8f01
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2020-11-06 13:01:23 +0900

    fix ministatic link

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "/api/v1": {
             target: "http://localhost:8000",
             onProxyReq: (proxyReq)  => {
-              proxyReq.setHeader("x-amzn-oidc-identity", "alice");
+              proxyReq.setHeader("x-amzn-oidc-identity", "bob");
               // proxyReq.setHeader("x-amzn-oidc-identity", "alice-x");
               // proxyReq.setHeader("X-Amzn-Oidc-Data", "dummy_header.eyJzdWIiOiJhbGljZS14IiwidXNlcm5hbWUiOiJQRVJNQU5fYWxpY2UteCJ9.dummy_signature");
             }
