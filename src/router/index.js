7f56e268286f91f614f9e0dd17c2c2d111124bc2
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2021-02-18 20:06:05 +0900

    support switch project

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import { commonRoute, appRoute } from './config'
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
+import store from '@/store'
+import axios from '@/axios'
 const routes = commonRoute.concat(appRoute)
 
 Vue.use(Router)
@@ -17,8 +19,25 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
-router.afterEach(() => {
+// Global after hook
+router.afterEach( async (to) => {
   NProgress.done()
+  if ( !to.query || !to.query.project_id || !store.state.user.user_id ) return
+  const admin = await axios.get('/iam/is-admin/?user_id=' + store.state.user.user_id ).catch((err) =>  {
+    return Promise.reject(err)
+  })
+  let q = 'project_id=' + to.query.project_id
+  if ( !admin.data.data.ok ) {
+    q += '&user_id=' + store.state.user.user_id
+  }
+  const res = await axios.get('/project/list-project/?' + q ).catch((err) =>  {
+    return Promise.reject(err)
+  })
+  if ( !res.data.data.project ) {
+    console.log( 'Unauthorized switch project: user_id=' + store.state.user.user_id + ', project_id=' + to.query.project_id )
+    return
+  }
+  store.commit('updateProject', res.data.data.project[0])
 })
 
 export default router
