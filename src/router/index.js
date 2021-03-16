a1c5f58fc02f12ef327c65588905692bb51bd0ff
gassara-kys <ogasawara_kiyoshi@cyberagent.co.jp>
2021-03-16 11:29:52 +0900

    fix project id process

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@ router.beforeEach( async (to, from, next) => {
   const project_id = to.query.project_id
   const current_project_id = store.state.project.project_id 
   const user_id = store.state.user.user_id
-  if ( typeof project_id != 'undefined' && typeof user_id != 'undefined' && project_id !== current_project_id ) {
+  if ( typeof project_id != 'undefined' && typeof user_id != 'undefined' && project_id != current_project_id ) {
     const admin = await axios.get('/iam/is-admin/?user_id=' + user_id )
     let q = 'project_id=' + project_id
     if ( !admin.data.data.ok ) {
@@ -30,8 +30,12 @@ router.beforeEach( async (to, from, next) => {
     }
     const res = await axios.get('/project/list-project/?' + q )
     if ( res.data.data.project ) {
-      store.commit('updateProject', res.data.data.project[0])
-      next({path: to.currentRoute, force: true})
+      await store.commit('updateProject', res.data.data.project[0])
+      // let query = Object.assign({}, to.query)
+      // delete query["project_id"]
+      // router.push({query: query}) // Edit query parameter
+      router.go({path: to.currentRoute})
+      next(false)
     }
   }
   next()
