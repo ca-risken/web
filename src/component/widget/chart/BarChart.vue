ad45636b9e5568217c338c002ab04a8d107468fb
iiiidaaa <oeriver@gmail.com>
2021-03-19 13:39:54 +0900

    add report visual

---
+++ src/component/widget/chart/BarChart.vue
@@ -0,0 +1,41 @@
+@@ -0,0 +1,40 @@
+<script>
+import { Bar, mixins } from 'vue-chartjs'
+import 'chartjs-plugin-colorschemes'
+const { reactiveProp } = mixins
+
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: () => {
+        return {
+          scales: {
+            xAxes: [{
+            stacked: true,
+              time: {
+                displayFormats: {
+                  day: 'MM/DD'
+                },
+                unit: 'day'
+              },
+            }],
+            yAxes: [{
+                stacked: true,
+            }],
+          },
+        }
+      }
+    },
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
