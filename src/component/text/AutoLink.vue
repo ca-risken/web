473449a771cbd4d1860b34a513b68d38d65b0026
gassara-kys <ogasawara_kiyoshi@cybernagent>
2021-12-01 23:57:53 +0900

    fis: recommend style

--- src/component/text/AutoLink.vue
+++ src/component/text/AutoLink.vue
@@ -8,18 +8,18 @@ export default {
     },
   },
   render: function (createElement) {
-    var a = this.text.split(/(https?:\/\/([\w!?/+-_~=;.,*&@#$%()']|\[\])+)/i)
-    var vnodes = a.map(function (x, i) {
+    let a = this.text.split(/(https?:\/\/[\w!?/+-_~=;.,*&@#$%()']+)/g)
+    let vnodes = a.map(function (x, i) {
       if (i % 2) {
         return createElement(
           'a',
           { attrs: { href: x, target: '_blank', rel: 'noopener' } },
           x
         )
-      } else if (!x) {
-        return this._v('')
-      } else {
+      } else if (x) {
         return this._v(x)
+      } else {
+        return this._v('')
       }
     }, this)
     return createElement('span', { attrs: { class: 'auto-link' } }, vnodes)
