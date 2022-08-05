ca587e4f103c8ae5d52b675b1fbcbb70be69c4e7
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-08-05 18:27:19 +0900

    add: datadog RUM setting

--- src/main.js
+++ src/main.js
@@ -6,6 +6,23 @@ import axios from '@/axios/'
 import vuetify from '@/plugin/vuetify'
 import interval from '@/plugin/interval'
 import i18n from '@/i18n'
+import { datadogRum } from '@datadog/browser-rum'
+
+// RUM
+if (process.env.VUE_APP_RUM_ID !== '' && process.env.VUE_APP_RUM_TOKEN !== '') {
+  datadogRum.init({
+    // parameters: https://docs.datadoghq.com/real_user_monitoring/browser/#configuration
+    applicationId: process.env.VUE_APP_RUM_ID,
+    clientToken: process.env.VUE_APP_RUM_TOKEN,
+    site: 'datadoghq.com',
+    service: 'risken',
+    sampleRate: 100,
+    premiumSampleRate: 100,
+    trackInteractions: true,
+    defaultPrivacyLevel: 'mask-user-input',
+  })
+  datadogRum.startSessionReplayRecording()
+}
 
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
