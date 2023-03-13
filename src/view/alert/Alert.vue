0e73601a57fd439e6882f2932882d9043f756f0a
iiiidaaa <iida_kazunori@cyberagent.co.jp>
2023-03-13 18:24:06 +0900

    fix alert

--- src/view/alert/Alert.vue
+++ src/view/alert/Alert.vue
@@ -193,21 +193,6 @@
                   <v-btn
                     text
                     link
-                    :to="{
-                      path: '/finding/finding/',
-                      query: { finding_id: id, from_score: 0 },
-                    }"
-                    color="indigo"
-                    class="px-1 mx-0"
-                    v-for="id of item.findingsIDs.slice(0, 5)"
-                    :key="id"
-                    risken-action-name="search-finding-by-finding-id-from-alert"
-                    >{{ id }}</v-btn
-                  >
-                  ...
-                  <v-btn
-                    text
-                    link
                     style="text-transform: none"
                     :to="{
                       path: '/finding/finding/',
@@ -216,7 +201,7 @@
                     color="grey darken-3"
                     class="px-1 mx-0"
                     risken-action-name="search-finding-by-alert-id-from-alert"
-                    >all ({{ item.findingsIDs.length }})</v-btn
+                    >all ({{ item.finding_history.count }})</v-btn
                   >
                 </v-col>
                 <v-col cols="2">
@@ -440,21 +425,16 @@ export default {
       const alertHistory = await this.listAlertHistory(this.alertModel.alert_id)
       alertHistory.forEach(async (history) => {
         const json = JSON.parse(history.finding_history)
-        let findingsIDs = []
-        if (json && json.finding_id) {
-          findingsIDs = json.finding_id
-        }
         this.alertHistoryModel.push({
           alert_history_id: history.alert_history_id,
           history_type: history.history_type,
           alert_id: history.alert_id,
           description: history.description,
           severity: history.severity,
-          finding_history: history.finding_history,
+          finding_history: json,
           project_id: history.project_id,
           created_at: history.created_at,
           updated_at: history.updated_at,
-          findingsIDs: findingsIDs,
         })
       })
     },
