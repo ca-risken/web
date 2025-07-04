24f9aaed5ef0fa2f7b390f8a9bc61628dabf3f13
Senk <44151180+senk8@users.noreply.github.com>
2025-07-04 16:52:08 +0900

    fix: nav guard

--- src/router/index.js
+++ src/router/index.js
@@ -27,17 +27,11 @@ router.beforeEach(async (to, from, next) => {
   const current_organization_id = store.state.organization.organization_id
   const user_id = store.state.user.user_id
 
-  // Handle organization_id changes
   if (
     typeof organization_id != 'undefined' &&
     typeof user_id != 'undefined' &&
     organization_id != current_organization_id
   ) {
-    // Change organization
-    console.log(
-      `Changing organization from ${current_organization_id} to ${organization_id}`
-    )
-
     const admin = await axios
       .get('/iam/is-admin/?user_id=' + user_id)
       .catch((err) => {
@@ -54,30 +48,22 @@ router.beforeEach(async (to, from, next) => {
         handleAPIError(err)
         return
       })
-    if (res.data.data.organization && res.data.data.organization.length > 0) {
-      // Find the specific organization by ID instead of always using the first one
-      const targetOrganization =
-        res.data.data.organization.find(
-          (o) => o.organization_id == organization_id
-        ) || res.data.data.organization[0]
-      console.log('Setting organization to:', targetOrganization)
-      await store.commit('updateOrganization', targetOrganization)
+    if (res.data.data.organization) {
+      await store.commit('updateOrganization', res.data.data.organization[0])
+      const query = {
+        ...to.query,
+        organization_id: store.state.organization.organization_id,
+      }
+      router.push({ query: query })
+      router.go({ path: to.currentRoute })
       next()
       return
-    } else {
-      console.warn('No organization found for ID:', organization_id)
     }
-  }
-
-  // Handle project_id changes
-  if (
+  } else if (
     typeof project_id != 'undefined' &&
     typeof user_id != 'undefined' &&
     project_id != current_project_id
   ) {
-    // Change project
-    console.log(`Changing project from ${current_project_id} to ${project_id}`)
-
     const admin = await axios
       .get('/iam/is-admin/?user_id=' + user_id)
       .catch((err) => {
@@ -92,42 +78,34 @@ router.beforeEach(async (to, from, next) => {
       handleAPIError(err)
       return
     })
-    if (res.data.data.project && res.data.data.project.length > 0) {
-      // Find the specific project by ID instead of always using the first one
-      const targetProject =
-        res.data.data.project.find((p) => p.project_id == project_id) ||
-        res.data.data.project[0]
-      console.log('Setting project to:', targetProject)
-      await store.commit('updateProject', targetProject)
+    if (res.data.data.project) {
+      await store.commit('updateProject', res.data.data.project[0])
+      const query = { ...to.query, project_id: store.state.project.project_id }
+      router.push({ query: query })
+      router.go({ path: to.currentRoute })
       next()
       return
-    } else {
-      console.warn('No project found for ID:', project_id)
     }
   }
 
-  // Force set organization_id parameter for organization routes
   if (
     to.path.startsWith('/organization') &&
+    !to.path.startsWith('/organization/new') &&
     !to.query.organization_id &&
     current_organization_id &&
     current_organization_id != ''
   ) {
-    let query = Object.assign({}, to.query)
-    query.organization_id = current_organization_id
+    const query = { ...to.query, organization_id: current_organization_id }
     next({ ...to, query })
     return
-  }
-
-  // Force set project_id parameter for non-organization routes
-  if (
-    !to.path.startsWith('/organization') &&
+  } else if (
+    to.path.startsWith('/project') &&
+    !to.path.startsWith('/project/new') &&
     !to.query.project_id &&
     current_project_id &&
     current_project_id != ''
   ) {
-    let query = Object.assign({}, to.query)
-    query.project_id = current_project_id
+    const query = { ...to.query, project_id: current_project_id }
     next({ ...to, query })
     return
   }
